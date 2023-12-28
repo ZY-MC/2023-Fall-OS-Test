f25de84b57428e07d7607b901e75f6044383f320
ZY_ <1575049303@qq.com>
2023-12-28 15:48:07 +0800

    21301037

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,14 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
+
+use core::arch::global_asm;
+
+extern crate alloc;
+
+#[macro_use]
+extern crate bitflags;
 
 #[macro_use]
 mod console;
@@ -12,8 +20,8 @@ mod loader;
 mod config;
 mod task;
 mod timer;
-
-use core::arch::global_asm;
+mod sync;
+mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -23,70 +31,24 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe { (a as *mut u8).write_volatile(0) }
-    });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8,
+            ebss as usize - sbss as usize,
+        ).fill(0);
+    }
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
+    mm::init();
+    println!("[kernel] back to world!");
+    mm::remap_test();
     trap::init();
-    println!("[kernel] Trap initialized.");
-    loader::load_apps();
-    println!("[kernel] Applications loaded.");
     trap::enable_timer_interrupt();
-    println!("[kernel] Enabled timer interrupt.");
     timer::set_next_trigger();
-    println!("[kernel] Have set next trigger.");
     task::run_first_task();
     panic!("Unreachable in rust_main!");
-}
-
-#[no_mangle]
-pub fn rust_main_expt4() -> ! {
-    clear_bss();
-    println!("[kernel] Hello, world!");
-    trap::init();
-    loader::load_apps();
-    task::run_first_task();
-    panic!("Unreachable in rust_main!");
-}
-
-#[no_mangle]
-pub fn rust_main_expt3() -> ! {
-    clear_bss();
-    println!("[Kernel] Hello, world!");
-    trap::init();
-    // batch::init();
-    // batch::run_next_app();
-    panic!("Unreachable in rust_main!");
-}
-
-#[no_mangle]
-pub fn rust_main_expt2() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
-    clear_bss();
-    println!("Hello, world!"); println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
 }
